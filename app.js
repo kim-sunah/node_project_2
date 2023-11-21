f25c13f8dda60ca224c91bdac31ebdac526a26be
sunah <1004_sunah@naver.com>
2023-11-22 06:50:59 +0900

    feat: DB모델정의
    -Sequelize 설정추가
    -Migration 코드추가
    -Model 코드추가

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { SERVER_PORT } from './constants/app.constans.js'
+import db from './models/index.cjs'
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const { products, users } = db;
+const product = await products.findAll();
+const user = await users.findAll();
+
+console.log({ product: product.map((product) => product.toJSON()), user: user.map((user) => user.toJSON()) })
+
 
 app.listen(SERVER_PORT, () => {
     console.log(`Example app listening on port ${SERVER_PORT}`)
---
+++ models/products.cjs
@@ -0,0 +1,55 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class products extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.users, { as: 'user' })
+    }
+  }
+  products.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    title: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    status: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'products',
+  });
+  return products;
+};
